670218e83397d5d1485ecf9da53aa1497cad6206
suchitra-das <sdas90933@gmail.com>
2026-03-04 17:39:11 +0530

    first file to practice

---
+++ arrays/array.js
@@ -0,0 +1,7 @@
+//Create an array of 5 fruits and print all fruits using a loop.
+
+const names = ["shivani", "suchi", "ashish", "sai", "arjun"];
+
+for (i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
