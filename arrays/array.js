02c868204728d34ca397a13e1665032146f0a37d
suchitra-das <sdas90933@gmail.com>
2026-03-12 00:16:45 +0530

    practice is more important in code

--- arrays/array.js
+++ arrays/array.js
@@ -5,3 +5,43 @@ const names = ["shivani", "suchi", "ashish", "sai", "arjun"];
 for (i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
+
+
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum ++
+}
+
+console.log(sum);
+
+
+
+
+const array = [1, 2, 3, 4, 5];
+
+let largest = array[0];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > largest) {
+    largest = array[i];
+  }
+}
+
+console.log(largest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ arrays/realwork/realwork.js
@@ -0,0 +1,246 @@
+//One
+
+
+let cart = [
+    {id:1,
+        name:"laptop",
+        price:250,
+        quanity:1
+    },
+    {id:2,
+        name:"mouse",
+        price:350,
+        quanity:2
+    
+    },
+        
+    {
+        id:3,
+        name:"keybord",
+        price: 500,
+        quanity:1
+    },
+]
+
+
+for(let product of cart){
+    console.log(product.name)
+}
+
+
+let total = 0;
+
+for(let product of cart){
+    total += product.price*product.quanity
+}
+
+console.log(total)
+
+
+let totals =  cart.reduce((sum,product)=>{
+    return  sum+product.price*product.quanity
+},0
+
+   
+)
+
+console.log(totals)
+
+
+
+for (let product of cart){
+ if( product.id === 2) {
+    product.quanity++
+ } 
+}
+
+console.log(cart)
+
+
+for (let i=0; i<cart.length; i++){
+    if(cart[i].id === 2){
+        cart[i].quanity++
+    }
+}
+
+console.log(cart)
+
+
+let cartRemove = cart.filter((item)=>item.id !== 3)
+
+console.log(cartRemove)
+
+
+cart.push({id:4,name:"apple", price:30000, quanity:1 })
+
+console.log(cart)
+
+
+let hprice = cart[0];
+
+for (let i=0; i<cart.length; i++){
+    if(cart[i].price > hprice.price){
+        hprice = cart[i]
+    }
+}
+
+console.log(hprice)
+
+
+for (let product of cart){
+   if(product.price> hprice.price){
+    hprice = product
+   } 
+}
+
+console.log(hprice)
+
+let lower = cart[0]
+for (let product of cart){
+   if(product.price< lower.price){
+    lower = product
+   } 
+}
+
+console.log(lower)
+
+
+let newCart = cart.map((item)=>item.name)
+console.log(newCart)
+
+
+let arrayNew =[]
+for(i=0; i<cart.length; i++){
+    if(cart[i].name){
+        arrayNew.push(cart[i].name)
+    }
+}
+
+console.log(arrayNew)
+
+
+let totalQuantity = 0
+
+for(let i=0; i<cart.length; i++){
+    totalQuantity += cart[i].quanity
+}
+
+console.log(totalQuantity)
+
+
+for (let product of cart){
+    totalQuantity+= product.quanity
+}
+
+console.log(totalQuantity)
+
+
+for (let i=0; i<cart.length; i++){
+    cart[i].price =cart[i].price* 0.9
+}
+
+console.log(cart)
+
+let isthere = false
+for(i=0; i<cart.length; i++){
+    if(cart[i].price >100){
+        console.log(cart[i])
+        isthere= true;
+        break
+    }
+}
+
+console.log(isthere)
+
+
+let update = cart.map((item)=>{
+    if(item.name === "laptop"){
+      return  {...item, price:45000}
+    }
+    return item
+})
+
+console.log(update)
+
+
+for (let i=0; i<cart.length; i++){
+    if(cart[i].name === "laptop"){
+        cart[i].price = 45000
+    }
+}
+
+console.log(cart)
+
+let isHere = false;
+
+for(let i=0; i<cart.length; i++){
+    if(cart[i].name === "mouse"){
+        isHere = true
+    }
+}
+
+console.log(isHere)
+
+
+
+cart.sort((a,b)=> a.price-b.price)
+console.log(cart)
+
+
+
+
+let obj ={}
+
+for(let key in cart){
+    obj[cart[key].id] = cart[key]
+}
+
+console.log(obj)
+
+
+let obj2 = {}
+
+
+for(let i=0; i<cart.length; i++){
+    let product = cart[i]
+
+obj2[product.id] = product
+}
+
+console.log(obj2)
+
+
+let count = 0;
+
+for(let product of cart){
+    count++
+}
+
+
+console.log(count)
+
+
+for (let i=0; i<cart.length; i++){
+    cart[i].inStock=true
+    
+}
+
+console.log(cart)
+
+  let totalCart = cart.map((item)=>{
+    return {
+ ...item, inStock:  true
+    }
+
+    
+})
+
+console.log(totalCart)
+
+
+for (let i=0; i<cart.length; i++){
+    if(cart[i].quanity === 0){
+        cart.slice(0,1)
+    }
+}
+
